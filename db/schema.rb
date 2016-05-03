26cd2542a04039b229727cb1e94a5bf5c2ff752a
Jeremy Garza <rangerjdg@gmail.com>
2016-05-03 17:08:07 -0500

    Fixed the intial none commits

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503205004) do
+ActiveRecord::Schema.define(version: 20160503212000) do
 
   create_table "collections", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20160503205004) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
