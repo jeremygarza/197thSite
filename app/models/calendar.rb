953abfb768f7526f2dacc5516754662a8148f6a0
Jeremy Garza <rangerjdg@gmail.com>
2016-05-05 10:28:48 -0500

    added views

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,6 +1,9 @@
 require 'csv'
 
 class Calendar < ActiveRecord::Base
+
+  belongs_to :user
+
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       Calendar.create!(Hash[row])
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Myapp::Application.routes.draw do
   devise_for :users
   get "home/index"
   get "home/minor"
+  get "home/supportPlt"
+  get "home/engineerPlt"
+  get "home/augmentationPlt"
+  get "home/maintenancePlt"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
