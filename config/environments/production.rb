c5a2ece4146472060fb8fdd80437807df6b931eb
Jeremy Garza <rangerjdg@gmail.com>
2016-05-05 13:41:26 -0500

    deleted web console

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,6 @@
 Myapp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.web_console.development_only = false
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
